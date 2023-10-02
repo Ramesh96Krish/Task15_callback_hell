d42685b9fa81c0b2675ebce680c1581e553d9454
Ramesh96Krish <109919808+Ramesh96Krish@users.noreply.github.com>
2023-10-02 20:36:17 +0530

    Callback hell

---
+++ script.js
@@ -0,0 +1,47 @@
+// countdown from 10 to 1 and say Happy Independence Day
+
+let h1 = document.createElement('h1')
+document.body.appendChild(h1)
+
+setTimeout(()=>{
+    h1.innerHTML='10'
+        setTimeout(()=>{
+            h1.innerHTML='9'
+
+            setTimeout(()=>{
+                h1.innerHTML='8'
+
+                setTimeout(()=>{
+                    h1.innerHTML='7'
+
+                    setTimeout(()=>{
+                        h1.innerHTML='6'
+
+                        setTimeout(()=>{
+                            h1.innerHTML='5'
+
+                            setTimeout(()=>{
+                                h1.innerHTML='4'
+
+                                setTimeout(()=>{
+                                    h1.innerHTML='3'
+
+                                    setTimeout(()=>{
+                                        h1.innerHTML='2'
+
+                                        setTimeout(()=>{
+                                            h1.innerHTML='1'
+
+                                            setTimeout(()=>{
+                                                h1.innerHTML='Happy Independence Day'
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },2000)
+},1000)
